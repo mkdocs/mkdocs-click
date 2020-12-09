5329db25bc487ba7ea54331039d3fc08ff741bf0
Florimond Manca <florimond.manca@datadoghq.com>
2020-12-09 11:20:51 +0100

    Pin dependencies to current major versions (#19)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     author_email="packages@datadoghq.com",
     license="Apache",
     packages=["mkdocs_click"],
-    install_requires=["click", "markdown"],
+    install_requires=["click==7.*", "markdown==3.*"],
     python_requires=">=3.7",
     include_package_data=True,
     zip_safe=False,
